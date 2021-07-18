b937b296d43f13ffbb322566c01c30a94f15ff2c
ashu020497 <ashu020497@gmail.com>
2021-07-18 11:40:49 +0530

    200SMA commit

--- Trial_Strategy/200SMA.py
+++ Trial_Strategy/200SMA.py
@@ -13,18 +13,22 @@ filePath = askopenfilename()
 root.withdraw()
 
 list = pd.read_excel(filePath)
-
+final_df = pd.DataFrame(columns={'Share','CMP','200SMA'})
 for i in list.index:
     share = str(list['Symbol'][i])
     share_token = str(list['Token'][i])
     df = pd.DataFrame(columns={'Date','Close'})
-    param = {'exchange':'NSE','symboltoken':share_token,'interval':'ONE_DAY','fromdate':'2020-07-08 09:15','todate':'2021-07-13 03:30'}
+    param = {'exchange':'NSE','symboltoken':share_token,'interval':'ONE_DAY','fromdate':'2020-07-08 09:15','todate':'2021-07-15 03:30'}
     stock_candle = obj.getCandleData(param)
     for x in stock_candle['data']:
         date,time = x[0].split('T')
         df = df.append({'Date':date,'Close':x[4]},ignore_index=True)
         df['200SMA'] = df['Close'].rolling(200).mean()
-    latest_close = float(df.iloc[-1,0])
+    latest_close = float(df.iloc[-1,1])
     latest_SMA = float(df.iloc[-1,2])
     if (latest_SMA*0.98) < latest_close < (latest_SMA*1.02):
-        print(share, ' near 200 Day SMA')
+        final_df = final_df.append({'Share':share,'CMP':latest_close,'200SMA':latest_SMA},ignore_index=True)
+
+writer = pd.ExcelWriter('200SMA.xlsx')
+final_df.to_excel(writer)
+writer.save()
